f76ab4566c6ce7ce794a355cded4bca68ca13151
kilua <matteom0797@me.com>
2021-03-26 23:04:03 +0100

    merge + mergeSort

--- Grafo.py
+++ Grafo.py
@@ -1,8 +1,8 @@
 class Grafo:
-    def __init__(self, n_nodi, n_archi, lista_nodi, lista_archi, lista_adiacenza):
+    def __init__(self, n_nodi, n_archi, lista_nodi, lista_archi, lista_adiacenza, lista_adiacenza_nodi):
         self.n_nodi = n_nodi
         self.n_archi = n_archi
         self.lista_nodi = lista_nodi
         self.lista_archi = lista_archi
         self.lista_adiacenza = lista_adiacenza #dizionario key: nodo, value: lista archi del nodo
-        #self.lista_adiacenza_nodi = lista_adiacenza_nodi #dizionario key: nodo, value: lista nodi adiacenti
+        self.lista_adiacenza_nodi = lista_adiacenza_nodi #dizionario key: nodo, value: lista nodi adiacenti
--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from Grafo import Grafo
 from Nodo import Nodo
 from Arco import Arco
 import os
+import math
 
 
 directory = "algoritmi-avanzati-laboratorio/mst_dataset"
@@ -13,6 +14,7 @@ def check_nodi(lista_grafi):
     for grafo in lista_grafi:
         nodi = len(grafo.lista_nodi) == int(grafo.n_nodi)
     return nodi
+
 
 
 #controllo archi
@@ -31,16 +33,16 @@ def check_generale():
     print("Il numero di nodi è giusto in ogni grafo? " + str(check_nodi(lista_grafi)))
     print("Il numero di archi è giusto in ogni grafo? " + str(check_archi(lista_grafi)))
     #controllo numr
-    
 
 
 
+#per fare testing ho messo solo il grafo 8_20
 def parsing():
     global directory
     for file in os.listdir(directory):
-        #if file == "input_random_01_10.txt":
-            #print(file)
-        crea_grafi(file)
+        if file == "input_random_08_20.txt":
+            crea_grafi(file)
+
 
 
 #funzione di servizio per fornire arco come lista di 3 valori
@@ -49,12 +51,14 @@ def getArco(arco):
     return [arco.nod1, arco.nodo2, arco.peso]
 
 
+
 def crea_grafi(path):
 
     global lista_grafi
     lista_nodi = set()
     lista_archi = []
     lista_adiacenza = {}
+    lista_adiacenza_nodi = {}
     
     f = open("algoritmi-avanzati-laboratorio/mst_dataset/" + path, "r")
 
@@ -73,9 +77,9 @@ def crea_grafi(path):
     f.close()
         
 
-
-    #creo i set di nodi
+    #creo i set di nodi e lista archi
     for i in range(0, len(lista_valori)):
+        #valori estratti dal file
         nodo_1 = lista_valori[i][0]
         nodo_2 = lista_valori[i][1]
         peso = lista_valori[i][2]
@@ -86,6 +90,8 @@ def crea_grafi(path):
         lista_nodi.add(nodo_2)
         lista_adiacenza.setdefault(nodo_1, [])    #inzializzo ogni chiave nodo a un valore list
         lista_adiacenza.setdefault(nodo_2, [])    #inzializzo ogni chiave nodo a un valore list
+        lista_adiacenza_nodi.setdefault(nodo_1, [])    
+        lista_adiacenza_nodi.setdefault(nodo_2, [])    
 
         #forse non serve una lista archi
         lista_archi.append(arco_1)
@@ -98,16 +104,74 @@ def crea_grafi(path):
 
 
     for i in range(0, len(lista_valori)):
-        lista_adiacenza[lista_valori[i][0]].append(Arco(lista_valori[i][0], lista_valori[i][1], lista_valori[i][2]))      #arco(u,v)
-        lista_adiacenza[lista_valori[i][1]].append(Arco(lista_valori[i][1], lista_valori[i][0], lista_valori[i][2]))      #arco(v,u)
+        nodo_1 = lista_valori[i][0]
+        nodo_2 = lista_valori[i][1]
+        peso = lista_valori[i][2]
+        lista_adiacenza_nodi[nodo_1].append(nodo_2)
+        lista_adiacenza_nodi[nodo_2].append(nodo_1)
+        lista_adiacenza[nodo_1].append(Arco(nodo_1, nodo_2, peso))      #arco(u,v)
+        lista_adiacenza[nodo_2].append(Arco(nodo_2, nodo_1, peso))      #arco(v,u)
 
-    lista_grafi.append(Grafo(n_nodi, n_archi, lista_nodi, lista_archi, lista_adiacenza))
+    lista_grafi.append(Grafo(n_nodi, n_archi, lista_nodi, lista_archi, lista_adiacenza, lista_adiacenza_nodi))
+
+
+
+#algoritmo merge
+# p, q, r sono indici dell'array tali che p <= q < r
+# gli indici dividono l'array in sottosequenze t.c. A[p..q] A[q+1..r] 
+def merge(array, p, q, r):
+    #lunghezza sottoarray A[p..q]
+    n1 = q-p+1
+    #lunghezza sottoarray A[q+1..r]
+    n2 = r-q
+    left = [0] * n1
+    right = [0] * n2
+    
+    for i in range(0, n1):
+        left[i] = array[p+i]
+    
+    for i in range(0, n2):
+        right[i] = array[q+i+1]
+    
+    i = 0
+    j = 0
+    k = p
+    while i < n1 and j < n2:
+        if left[i] <= right[j]:
+            array[k] = left[i]
+            i += 1
+        else:
+            array[k] = right[j]
+            j += 1
+        k += 1
+    #elementi rimanenri di left e right
+    #non sono riuscito ad implementarlo con il valore infito del libro :(
+    while i < n1:
+        array[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        array[k] = right[j]
+        j += 1
+        k += 1
+
+    
+
+def mergeSort(array, p, r):
+    if p < r:
+        q = (p+r)//2
+        mergeSort(array, p, q)
+        mergeSort(array, q+1, r)
+        merge(array, p, q, r)
+        
 
 
 
 
 parsing()
-check_generale()
+for k,v in lista_grafi[0].lista_adiacenza_nodi.items():
+    print(k,v)
 
 
 
